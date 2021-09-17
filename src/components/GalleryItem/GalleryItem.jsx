7a936049a63bad911a22467bd0309ae74bf682b5
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-09-17 16:17:17 -0500

    description now appears over photo on click

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from 'react';
 import './GalleryItem.css'
 
 function GalleryItem(props) {
+    let [clickCheck, setClickCheck] = useState(0);
     return(
         <>
             <div className="picture-like-container">
                 <div className="image-div">
-                    <img src={props.path}></img>
+                    <img src={props.path} 
+                    onClick={() => clickCheck === 0 ? setClickCheck(1) : setClickCheck(0)}
+                    className={clickCheck===0 ? "" : "grayed-out"}
+                    ></img>
+                    <p className="imgText"
+                    onClick={() => clickCheck === 0 ? setClickCheck(1) : setClickCheck(0)}>
+                    {clickCheck===0 ? "" : props.description}</p>
                 </div>
                 <div className="like-container">
-                    <button onClick={() => props.addLike(props.id)}>Like</button>
+                    <button onClick={() => props.addLike(props.id)}>Love It!</button>
                     
                     
                     <p>{props.likes == 0 ? "Nobody loves this" : 
