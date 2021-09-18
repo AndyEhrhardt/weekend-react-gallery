0a1b7ae3d43013738346a5b3c7ca4fdf068a86b3
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-09-17 19:35:25 -0500

    post route adds picture info to database

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,7 @@ import InputForm from '../InputForm/InputForm.jsx'
 
 function App() {
   let [pictures, setPictures] = useState([]);
-  let [pathInput, setPathInput] = useState([]);
+  let [fileNameInput, setFileNameInput] = useState([]);
   let [descriptionInput, setDescriptionInput] = useState([]);
 
   useEffect(() => {
@@ -36,6 +36,30 @@ function App() {
     })
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('in handle submit')
+    console.log(fileNameInput)
+    if (fileNameInput.length === 0 || descriptionInput.length === 0){
+      alert("Please enter a file name and description")
+    }else{
+      postInputs();
+    }
+  }
+
+  const postInputs = () => {
+    axios.post('/gallery', {path: `images/${fileNameInput}`, description: descriptionInput})
+    .then(response =>{
+      setFileNameInput('');
+      setDescriptionInput('');
+      getPictures();
+    })
+    .catch(error => {
+      alert('Error adding picture')
+      console.log(error);
+    })
+  }
+
 
   return (
     <div className="App">
@@ -43,8 +67,9 @@ function App() {
         <h1 className="App-title">Gallery of My Life</h1>
       </header>
       <InputForm 
-      setPathInput={setPathInput}
+      setFileNameInput={setFileNameInput}
       setDescriptionInput={setDescriptionInput}
+      handleSubmit={handleSubmit}
       />
       <GalleryList 
         pictures={pictures}
--- src/components/InputForm/InputForm.jsx
+++ src/components/InputForm/InputForm.jsx
@@ -1,7 +1,27 @@
-function InputForm(props){
+function InputForm(props){  
     return(
         <>
-            
+        <form onSubmit={() => {props.handleSubmit}}>
+            <label>
+                File Name
+            </label>
+            <input
+                type="text"
+                placeholder="File Name"
+                value={props.fileNameInput}
+                onChange={(evt) => props.setFileNameInput(evt.target.value)}
+            />
+            <label>
+                Description
+            </label>
+            <input
+                type="text"
+                placeholder="Description"
+                value={props.descriptionInput}
+                onChange={(evt) => props.setDescriptionInput(evt.target.value)}
+            />
+            <button type="submit" onClick={props.handleSubmit}>Add Picture</button>
+        </form>
         </>
     )
 }
